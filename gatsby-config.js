ba1d8de306553bd97fc6af78b0c2ef33700f2dc2
unknown <lubnanaqvi11@gmail.com>
2020-05-03 22:19:55 -0400

    package-lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
     title: `Coalition of Muslim Women KW`,
     description: ``,
     author: `@gatsbyjs`,
   },
-  pathPrefix: `/cmw\-kw\.org`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-htaccess`,
@@ -36,6 +37,13 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID || "",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
